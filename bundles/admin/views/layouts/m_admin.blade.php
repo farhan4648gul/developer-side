9275af2d57cf93b3d65e86ff42ae0fc62efa5252
joharijumali <joharijumali@gmail.com>
2013-05-28 08:22:17 +0000

    mobile layout fix footer

--- bundles/admin/views/layouts/m_admin.blade.php
+++ bundles/admin/views/layouts/m_admin.blade.php
@@ -7,14 +7,14 @@
 </head>
 
 <body>
-<div data-role="login" data-theme="d" id="container" data-url="container">
-	<div data-role="header" data-theme="d" data-position-fixed="true">
+<div data-role="page" data-theme="d" id="container" data-url="container">
+	<div data-role="header" data-theme="d" data-fullscreen="true">
 		<h4><small>3FDS Admnistrator Panel</small></h4>
 	</div>
 	<div data-role="content" data-theme="d" >
 		@yield('login')
 	</div>
-	<div data-role="footer" data-theme="d" ><h4><small>Copyright© 2013</small></h4></div>
+	<div data-role="footer" data-theme="d"  data-position="fixed" ><h4><small>Copyright© 2013</small></h4></div>
 </div>
 	<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
 	<script src="http://code.jquery.com/mobile/1.3.1/jquery.mobile-1.3.1.min.js"></script>
