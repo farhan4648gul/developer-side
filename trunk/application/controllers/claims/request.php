f144ae1abeecb2ff65cdc8621adfc4a21d72c633
joharijumali@gmail.com <joharijumali@gmail.com@c5aebf30-8ea1-3613-f6ab-09ae8371c5c0>
2013-06-23 19:50:19 +0000

    claims application controller

--- trunk/application/controllers/claims/request.php
+++ trunk/application/controllers/claims/request.php
@@ -1,68 +1,176 @@
 <?php
 
+use Data_Claims_Cat as Cat;
+
 class Claims_Request_Controller extends Base_Controller {
 
-	public function action_index()
+	public $restful = true;
+
+	public function get_index()
 	{
-		return View::make('claims.request.approval');
+		$data['claimlist'] = Claims_App::listClaims();
+
+		return View::make('claims.request.index',$data);
 	}
 
-	public function action_request()
+	/*
+	* Claims application form
+	*/
+	public function get_request()
 	{
-		return View::make('claims.request.request');
+
+		$data['claimsCat'] = Cat::arrayCats();
+
+		return View::make('claims.request.request',$data);
 	}
 
-	public function action_requestDetail()
+	public function post_request()
+	{
+		$input = Input::all();
+
+		$claimID = Claims_App::applyclaims($input);
+
+		return Redirect::to('claims/request/requestDetail/'.$claimID);
+	}
+
+	public function get_requestDetail()
 	{
 
-		$data['body'] = array(
-		    array(
-		        'id' => '1', 
-		        'count'=> '1',
-		        'fname' => 'Patrick', 
-		        'gname' => 'Patrick', 
-		        'yname' => 'Patrick', 
-		        'jname' => 'Patrick', 
-		        'lname' => 'Patrick', 
-		        'pname' => 'Patrick', 
-		        'kname' => 'Patrick', 
-		        'lname' => 'Talmadge'
-		    ),array(
-		        'id' => '2', 
-		        'count'=> '2',
-		        'fname' => 'Patrick', 
-		        'gname' => 'Patrick', 
-		        'yname' => 'Patrick', 
-		        'jname' => 'Patrick', 
-		        'lname' => 'Patrick', 
-		        'pname' => 'Patrick', 
-		        'kname' => 'Patrick', 
-		        'lname' => 'Talmadge'
-		    ),array(
-		        'id' => '3', 
-		        'count'=> '3',
-		        'fname' => 'Patrick', 
-		        'gname' => 'Patrick', 
-		        'yname' => 'Patrick', 
-		        'jname' => 'Patrick', 
-		        'lname' => 'Patrick', 
-		        'pname' => 'Patrick', 
-		        'kname' => 'Patrick', 
-		        'lname' => 'Talmadge'
-		    ),
-		);
+		$data['claimID'] = $claimID = URI::segment(4);
+
+		$data['app'] = $claimApp = Claims_App::appInfo($claimID);
+		$data['detailList'] = Claims_App::listDetails($claimID);
+
 
 		return View::make('claims.request.requestdetail',$data);
 	}
 
-	public function action_approval()
+	public function post_addDetail(){
+
+		$input = Input::get();
+		$claimID = $input['claimid'];
+		$detailID = $input['detailId'];
+        $input['detaildate'] = date('Y-m-d',strtotime($input['detaildate']));
+
+        // echo "<pre>";print_r($input);die;
+        unset($input['claimid']);
+        unset($input['detailId']);
+
+        if(!isset($detailID) || $detailID == NULL){
+        	$claimDetails = Claims_App::find($claimID);
+ 			$claimDetails->detail()->insert($input);
+        }else{
+        	$claimDetails = Claims_Detail::find($detailID);
+        	$claimDetails->fill($input);
+        	$claimDetails->save();
+        }
+        
+
+		return Claims_App::listDetails($claimID);
+
+	}
+
+	public function post_upload(){
+		
+		$input = Input::get();
+		$file = Input::file('fileInput');
+
+		$separator = ($input['claimdetailid'] != NULL )? $input['claimid'].'/'.$input['claimdetailid'] : $input['claimid'];
+
+        $extension = File::extension($file['name']);
+        $directory = 'upload/claims/'.sha1(Auth::user()->userid).'/'.str_replace("-", "", date('Y-m-d')).'/'.$separator; 
+        $filename = Str::random(16, 'alpha').time().".{$extension}";
+
+        if(!is_dir(path('public').$directory)){
+            mkdir(path('public').$directory, 0777,true);
+        }
+
+        $maxSize = ini_get('upload_max_filesize')*1024*1024*1024;
+
+        if($file['size'] != null && $file['size'] < $maxSize){
+            try{
+                $upload_success = Input::upload('fileInput', path('public').$directory, $filename);
+
+                if($upload_success){
+
+                    $input['recpath'] = $directory.'/'.$filename;
+
+                    $receipt = new Claims_Receipt;
+                    $receipt->fill($input);
+                    $receipt->save();
+
+                    Log::write('Claims Receipt', 'File Uploaded : ' . $filename.' by '.Auth::user()->username);
+
+                    return $directory.'/'.$filename;
+                }
+
+            }catch (Exception $e) {
+                Log::write('Claims Receipt', 'Upload error: ' . $e->getMessage());
+            }
+        }else{
+            Log::write('Claims Receipt', 'Upload error: Exceed max size '.ini_get('upload_max_filesize'));
+        }
+	}
+
+    public function post_deleteDetail(){
+        
+        $input = Input::get();
+
+        Claims_Detail::find($input['id'])->delete();
+
+        $files = Claims_Receipt::where('claimdetailid','=',$input['id'])->where('claimid','=',$input['claimid'])->get();
+
+        foreach ($files as $record) {
+        	$fileLoc = path('storage').$record->recpath;
+        	if(file_exists ( $fileLoc )){ unlink($fileLoc); }
+
+        }	
+
+		Claims_Receipt::where('claimdetailid','=',$input['id'])->where('claimid','=',$input['claimid'])->delete();
+
+        return Claims_App::listDetails($input['claimid']);
+    }
+
+    public function get_detailinfo(){
+
+        $input = Input::get();
+
+        $data = Claims_Detail::detailInfo($input['id']);
+
+        return json_encode($data);
+    }
+
+    public function get_receipt(){
+
+    	$input = Input::get();
+
+    	$data = Claims_Receipt::receiptList($input['id']);
+
+        return json_encode($data);
+    }
+
+    public function post_submitClaims(){
+
+        $input = Input::get();
+
+        /* code to update application status*/
+
+        $url = 'claims/request/index';
+
+        return $url;
+    }
+
+
+	public function get_approval()
 	{
 		return View::make('claims.request.approval');
 	}
 
-	public function action_history()
+	public function get_history()
 	{
-		return View::make('claims.request.history');
+		$data['claimlist'] = Claims_App::listClaims();
+
+		return View::make('claims.request.history',$data);
 	}
 }
 
