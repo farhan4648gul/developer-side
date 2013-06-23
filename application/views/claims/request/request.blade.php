a17b46d18dc45155b42465c7f3729e43178fadfd
joharijumali <joharijumali@gmail.com>
2013-06-23 19:47:28 +0000

    claims application

--- application/views/claims/request/request.blade.php
+++ application/views/claims/request/request.blade.php
@@ -5,23 +5,18 @@
 	  <h3>Claims Request Form</h3>
 	</div>
 
-  {{ Form::open('admin/auth/authenticate', 'POST', array('class' => 'form-horizontal')) }}
+  {{ Form::open('claims/request/request', 'POST', array('class' => 'form-horizontal')) }}
     <div class="control-group">
-        {{ Form::label('type', 'Claims Category', array('class' => 'control-label')) }}
+        {{ Form::label('claimscat', 'Claims Category', array('class' => 'control-label')) }}
       <div class="controls">
-        {{ Form::select('type', array('1' => 'Mileage/Travel', '2' => 'Lodging'), '1',array('class' => 'span4', 'required')); }}
-      </div>
-    </div>
-    <div class="control-group">
-      {{ Form::label('date', 'Date Apply', array('class' => 'control-label')) }}
-      <div class="controls ">
-        {{ Form::text('date', '', array('class' => 'span4','placeholder'=>'16/4/2013', 'required'));}}
+        {{ Form::select('claimscat', $claimsCat , '0',array('class' => 'span4', 'required')); }}
       </div>
     </div>
     <div class="control-group">
       {{ Form::label('month', 'Month Apply For', array('class' => 'control-label')) }}
       <div class="controls">
-        {{ Form::text('month', '', array('class' => 'span4','placeholder'=>'January 2013', 'required'));}}
+        {{ Form::text('applydate', '', array('class' => 'span4','placeholder'=>'16/4/2013', 'required', 'id' => 'applydate'));}}
+        {{ Form::text('applymonth', '', array('class' => 'span4','placeholder'=>'January 2013', 'required','id' => 'applymonth' ));}}
       </div>
     </div>
 	<div class="form-actions ">
@@ -31,4 +26,17 @@
 	
   <!-- </form> -->
   {{ Form::close()}}
+@endsection
+@section('scripts')
+<script>
+$(function() {
+  $( "#applydate" ).datepicker({
+      showWeek: true,
+      firstDay: 1,
+      altField: "#applymonth",
+      altFormat: "MM, yy",
+      showButtonPanel: true
+    });
+  });
+</script>
 @endsection
