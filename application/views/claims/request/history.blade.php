81fd1c29a8bb2e3facb340f9558a6a5dfa33c4d1
joharijumali <joharijumali@gmail.com>
2013-06-23 19:48:10 +0000

    claims application

--- application/views/claims/request/history.blade.php
+++ application/views/claims/request/history.blade.php
@@ -1,7 +1,10 @@
 @layout('layouts.system')
 
 @section('content')
-	<div class="page-header">
-	  <h3>Claims Request History</h3>
-	</div>
+<div class="page-header">
+  	<h3>Claims Application History</h3>
+</div>
+<div id="dataGroupList" class="rows-fluid show-grid">
+	{{ $claimlist }}
+</div>
 @endsection
