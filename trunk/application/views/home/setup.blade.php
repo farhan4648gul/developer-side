54232fe746290d31df49f33afcb8933993c55336
joharijumali@gmail.com <joharijumali@gmail.com@c5aebf30-8ea1-3613-f6ab-09ae8371c5c0>
2013-06-24 04:07:52 +0000

    setup page

--- trunk/application/views/home/setup.blade.php
+++ trunk/application/views/home/setup.blade.php
@@ -1,6 +1,7 @@
 <div class="row-fluid span12" >
-<h1>Use Artisan to configure setup</h1>
-<h2>use command :</h2>
-<h3><small> php artisan Setup driver=<em>drivername</em> databases=<em>databasename</em> username=<em>usernamedb</em> password=<em>pasworddb</em> host=<em>hosting</em> </small></h3>
+<h1>Use terminal/command line to configure setup</h1>
+<h2>Reminder: Make sure you have create your database first before run command</h2>
+<h2>run command :</h2>
+<h3><small> php artisan Setup driver=<em>your drivername</em> database=<em>your databasename</em> uname=<em>your usernamedb</em> pword=<em>your pasworddb</em> host=<em>your hosting</em> username=<em>your admin username</em> password=<em>your admin password</em> </small></h3>
 <h3>Refresh page after setup finish</h3>
 </div>
