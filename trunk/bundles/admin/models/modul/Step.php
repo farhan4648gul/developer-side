4b376b8af942a493735fedde8fa8471770774e4c
joharijumali@gmail.com <joharijumali@gmail.com@c5aebf30-8ea1-3613-f6ab-09ae8371c5c0>
2013-07-02 07:19:02 +0000

    pagination update

--- trunk/bundles/admin/models/modul/Step.php
+++ trunk/bundles/admin/models/modul/Step.php
@@ -14,7 +14,7 @@ class Step extends Eloquent {
 
         $flowSteps = Step::left_join('users_roles AS r', 'flows_steps.roleid', '=', 'r.roleid')
         			->where('flowid','=',$flowid)
-        			->get(array('r.role', 'flows_steps.*'));
+        			->paginate(10,array('r.role', 'flows_steps.*'));
 
         $datagrid = new Datagrid;
         $datagrid->setFields(array('step' =>'Step'));
@@ -22,8 +22,8 @@ class Step extends Eloquent {
         $datagrid->setFields(array('next' =>'Next Action'));
         $datagrid->setFields(array('condition1' =>'Condition Action 1'));
         $datagrid->setFields(array('condition2' =>'Condition Action 2'));
-        $datagrid->setFields(array('created_at' =>'Created'));
-        $datagrid->setFields(array('updated_at' =>'Updated'));
+        // $datagrid->setFields(array('created_at' =>'Created'));
+        // $datagrid->setFields(array('updated_at' =>'Updated'));
         $datagrid->setAction('edit','editStep',true,array('stepid'));
         $datagrid->setAction('delete','deleteStep',true,array('stepid'));
         $datagrid->setTable('steps','table table-bordered table-hover table-striped table-condensed');
