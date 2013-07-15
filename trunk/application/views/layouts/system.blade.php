80b48276c1b6343f96d6dbbb911b9a99d9e6562a
joharijumali@gmail.com <joharijumali@gmail.com@c5aebf30-8ea1-3613-f6ab-09ae8371c5c0>
2013-07-15 05:56:53 +0000

    update fixedtop plugin

--- trunk/application/views/layouts/system.blade.php
+++ trunk/application/views/layouts/system.blade.php
@@ -4,7 +4,6 @@
     <title>3F Developer Suite</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap -->
-    <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
     {{ Asset::container('bootstrapper')->styles() }}
   </head>
   <body>
@@ -14,16 +13,22 @@
       <div class="span12">
         @include('plugins.fixedTop')
         <div class="container-fluid">
-          <div class="row">&nbsp;</div>
+          <!-- <div class="row">&nbsp;</div> -->
           <div class="row-fluid">
             <div class="span12">
               
               <div class="row-fluid">
                 <div class="span2">
                   <!--Sidebar content-->
-                  {{ $sidebar }}
+                  <div class="row">&nbsp;</div>
+                  <aside>{{ $sidebar }}</aside>
+                  <div class="pull-left">
+                    <div id="date"></div>
+                  </div>
+                  
                 </div>
                 <div class="span10">
+                  <!-- <div class="row-fluid">{{Breadcrumb::create(array('Home' => '#', 'Library' => '#', 'Data'));}}</div> -->
                   <!--Body content-->
                   @yield('content')
                 </div>
@@ -33,16 +38,122 @@
         </div>
       </div>
     </div>
-    <div id="actionStatModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-      <div class="modal-body">
-        <div id="actionStat"></div>
-      </div>
-      <!-- <div class="modal-footer">
-        <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
-      </div> -->
-    </div>
     {{ Asset::container('bootstrapper')->scripts() }}
-    <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+    <script>
+
+    startTime();
+
+    function startTime()
+    {
+      var today=new Date();
+      var h=today.getHours();
+      var m=today.getMinutes();
+      var s=today.getSeconds();
+      var day=today.toDateString();
+      // add a zero in front of numbers<10
+      m=checkTime(m);
+      s=checkTime(s);
+      $('#date').empty().html(day);
+      $('#date').append(' '+h+":"+m+":"+s);
+      t=setTimeout(function(){startTime()},500);
+    }
+
+    function checkTime(i)
+    {
+      if (i<10){
+        i="0" + i;
+      }
+      return i;
+    }
+
+      function profileInfo(url){
+
+        $('#profileModal').modal('toggle');
+
+        $.get(url, function(data,status){
+
+          for (x in data)
+          {   
+            $('#updateProfileForm input[name="'+ x +'"]' ).val(data[x]);
+          }
+
+          },"json");
+
+      }
+
+
+      $('#editProfileBtn').click(function() {
+        
+        $.post('{{url("console/user/profile")}}', $("#updateProfileForm").serialize(),function(data) {
+                sourcedata = data;
+              }).success(function() { 
+              
+              for (x in sourcedata)
+              {   
+                $('#updateProfileForm input[name="'+ x +'"]' ).val(sourcedata[x]);
+              }
+
+              var notfail = '<div class="alert alert-success" >' +
+                    '<button type="button" class="close" data-dismiss="alert">&times;</button>' +
+                    '<strong>Update Success!!!</strong> Profile information successfully updated.</div>'
+
+
+              $('.modal-body').prepend(notfail);
+
+              }).fail(function() { 
+
+              var notfail = '<div class="alert alert-error" >' +
+                    '<button type="button" class="close" data-dismiss="alert">&times;</button>' +
+                    '<strong>Update Fail!!!</strong> Profile information update failed.</div>'
+
+
+              $('.modal-body').prepend(notfail);
+
+              });
+
+      });
+
+
+
+      $('#resetBtn').click(function() {
+        
+        $.post('{{url("console/user/resetpassword")}}', $("#resetForm").serialize(),function(data) {
+                sourcedata = data;
+              }).success(function() { 
+
+              var notfail = '<div class="alert alert-success" >' +
+                    '<button type="button" class="close" data-dismiss="alert">&times;</button>' +
+                    '<strong>Update Success!!!</strong> Password successfully updated.</div>'
+
+
+              $('#resetModalBody').prepend(notfail);
+              $('#resetForm :input').val('');
+
+              }).fail(function() { 
+
+              var notfail = '<div class="alert alert-error" >' +
+                    '<button type="button" class="close" data-dismiss="alert">&times;</button>' +
+                    '<strong>Update Fail!!!</strong> Password update failed.</div>'
+
+
+              $('#resetModalBody').prepend(notfail); 
+
+              });
+
+
+      });
+
+
+    </script>
+    <script type="text/javascript">
+      $(document).ajaxStart(function() {
+        $('body').modalmanager('loading');
+      });
+      $(document).ajaxComplete(function() {
+        $('body').modalmanager('loading');
+      });
+
+    </script>
     @yield('scripts')
   </body>
 </html>
