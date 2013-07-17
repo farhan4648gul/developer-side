94c065014c6520770613671852b66679998779a8
joharijumali@gmail.com <joharijumali@gmail.com@c5aebf30-8ea1-3613-f6ab-09ae8371c5c0>
2013-07-17 01:41:19 +0000

    modification for page recomposing

--- trunk/bundles/admin/views/modul/page.blade.php
+++ trunk/bundles/admin/views/modul/page.blade.php
@@ -5,13 +5,49 @@
     <h3>Pages Setup</h3>
 </div>
 <div class="row-fluid">
+	<div id="container" class="container-fluid">
+	</div>
+</div>
+<div class="row-fluid">
+	<a href="#" role="button" onclick="recompose()" data-loading-text="Loading..." class="btn pull-right" style='margin-bottom:10px'><i class="icon-plus"></i>&nbsp;Recompose Page</a>
+</div>
+<div class="row-fluid">
   <div class="span12">
     {{ Form::open('admin/modul/page', 'POST') }}
-    {{ $struct }}
+    <div id="struct">{{ $struct }}</div>
     <div class="form-actions">
 	    <button type="submit" class="btn pull-right btn-primary">Save changes</button>
   	</div>
     {{ Form::close()}}
   </div>
 </div>
+@endsection
+@section('scripts')
+<script>
+
+	function recompose(){
+		
+		$.post('{{ url('admin/modul/recomp'); }}', function(data) {
+			      sourcedata = data;
+			    }).success(function() { 
+					var notfail = '<div class="alert alert-success" >' +
+								'<button type="button" class="close" data-dismiss="alert">&times;</button>' +
+								'<strong>Composing Modul File Completed !!!</strong></div>'
+
+
+					$('#container').prepend(notfail); 
+					$( "#struct" ).empty().append( sourcedata );
+			    }).fail(function() { 
+
+					var fail = '<div class="alert alert-error" >' +
+								'<button type="button" class="close" data-dismiss="alert">&times;</button>' +
+								'<strong>Composing Modul File Fail!!!</strong></div>'
+
+
+					$('#container').prepend(fail); 
+
+			    });
+
+	}
+</script>
 @endsection
