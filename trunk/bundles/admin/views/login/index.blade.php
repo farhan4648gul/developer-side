cc9c35cc6ca10a1cb9b2f1ccc4951626a21a7ed1
joharijumali@gmail.com <joharijumali@gmail.com@c5aebf30-8ea1-3613-f6ab-09ae8371c5c0>
2013-07-17 01:41:58 +0000

    modify view admin login page

--- trunk/bundles/admin/views/login/index.blade.php
+++ trunk/bundles/admin/views/login/index.blade.php
@@ -6,7 +6,7 @@
 @endif
 <center>
 {{Form::open('admin/login', 'POST', array('class'=>'form-actions','data-ajax'=>'false','style'=>'border:1px solid #E5E5E5;margin:0px auto;padding:19px 29px 29px;max-width:300px;box-shadow:0 1px 2px rgba(0, 0, 0, 0.05)'))}}
-<h3>3F Developer Login</h3>
+<h3>System Admin Login</h3>
 {{ Form::span12_text('username',null,array('placeholder'=>'Username','required')) }}
 {{ Form::span12_password('password',array('placeholder'=>'Password','required')) }}
 {{	Form::submit('Login', array('class' => 'btn btn-success'))}}
