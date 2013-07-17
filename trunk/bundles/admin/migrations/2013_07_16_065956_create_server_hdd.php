f595e9220b319dae42a5d627dabcd2899e9f9d5c
joharijumali@gmail.com <joharijumali@gmail.com@c5aebf30-8ea1-3613-f6ab-09ae8371c5c0>
2013-07-17 01:45:24 +0000

    add migration

---
+++ trunk/bundles/admin/migrations/2013_07_16_065956_create_server_hdd.php
@@ -0,0 +1,48 @@
+<?php
+
+class Admin_Create_Server_Hdd {
+
+	/**
+	 * Make changes to the database.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('server_hdd', function($table) {
+
+			$table->engine = 'InnoDB';
+
+		    $table->increments('hdd_id');
+		    $table->integer('server_id');
+		    $table->string('hdd_name', 100);
+		    $table->string('hdd_free', 100);
+		    $table->string('hdd_total', 100);
+		    $table->string('hdd_rrqms', 100);
+		    $table->string('hdd_wrqms', 100);
+		    $table->string('hdd_rs', 100);
+		    $table->string('hdd_ws', 100);
+		    $table->string('hdd_rMBs', 100);
+		    $table->string('hdd_wMBs', 100);
+		    $table->string('hdd_avgrq', 100);
+		    $table->string('hdd_avgqu', 100);
+		    $table->string('hdd_await', 100);
+		    $table->string('hdd_svctm', 100);
+		    $table->string('hdd_util', 100);
+		    $table->date('hdd_date');
+		    $table->timestamps();
+
+		});
+	}
+
+	/**
+	 * Revert the changes to the database.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('server_hdd');
+	}
+
+}
