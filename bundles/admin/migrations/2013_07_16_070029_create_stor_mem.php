8f9e3c10d4e709569133774baeefc0d536128c76
joharijumali <joharijumali@gmail.com>
2013-07-17 01:44:34 +0000

    add migration

---
+++ bundles/admin/migrations/2013_07_16_070029_create_stor_mem.php
@@ -0,0 +1,42 @@
+<?php
+
+class Admin_Create_Stor_Mem {
+
+	/**
+	 * Make changes to the database.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('stor_mem', function($table) {
+
+			$table->engine = 'InnoDB';
+
+		    $table->increments('mem_id');
+		    $table->integer('server_id');
+		    $table->string('mem_kbmemfree', 100);
+		    $table->string('mem_kbmemused', 100);
+		    $table->string('mem_kbbuffers', 100);
+		    $table->string('mem_kbcached', 100);
+		    $table->string('mem_kbswpfree', 100);
+		    $table->string('mem_kbswpused', 100);
+		    $table->string('mem_swpused', 100);
+		    $table->string('mem_kbswpcad', 100);
+		    $table->date('mem_date');
+		    $table->timestamps();
+
+		});
+	}
+
+	/**
+	 * Revert the changes to the database.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('stor_mem');
+	}
+
+}
