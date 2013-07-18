aac0a7a63db7c3977c6ff993afe5837ea8b115cf
joharijumali@gmail.com <joharijumali@gmail.com@c5aebf30-8ea1-3613-f6ab-09ae8371c5c0>
2013-07-18 04:12:01 +0000

    modify confirmation page after init lang

--- trunk/application/views/home/confirm.blade.php
+++ trunk/application/views/home/confirm.blade.php
@@ -1,9 +1,9 @@
-@layout('layouts.login')
+@layout('layouts.setup')
 
-@section('login')<center>
-  {{ Form::open('console/auth/verifyupdate', 'POST', array('class'=>'form-actions','style'=>'border:1px solid #E5E5E5;margin:100px auto;padding:19px 29px 29px;max-width:300px;box-shadow:0 1px 2px rgba(0, 0, 0, 0.05)')) }}
-  {{ Form::hidden('key',URI::segment(2)) }}
-  <h3>3F Developer Suite</h3>
+@section('setup')<center>
+  {{ Form::open('console/auth/verifyupdate', 'POST', array('class'=>'form-actions','style'=>'border:1px solid #E5E5E5;margin:50px auto;padding:19px 29px 29px;max-width:400px;box-shadow:0 1px 2px rgba(0, 0, 0, 0.05)')) }}
+  {{ Form::hidden('key',URI::segment(3)) }}
+  <h3>User Registration Confirmation</h3>
   <div class="span12">{{ Label::important('Update your user registration preferences'); }}</div>
   
   {{ Form::xlarge_text('username',null,array('placeholder'=>'New Username','required')) }}
