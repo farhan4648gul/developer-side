28f3f3bcf58c520e345832537a4497b334b224a2
joharijumali@gmail.com <joharijumali@gmail.com@c5aebf30-8ea1-3613-f6ab-09ae8371c5c0>
2013-07-18 04:54:39 +0000

    add language modifier

---
+++ trunk/application/language/my/admin.php
@@ -0,0 +1,28 @@
+<?php 
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Admin Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are used by the paginator library to build
+	| the pagination links. You're free to change them to anything you want.
+	| If you come up with something more exciting, let us know.
+	|
+	*/
+
+	'monitor' => '',
+	'dashboard'     => 'Paparan Utama',
+	'sysmgmt'     => 'Konfigurasi SISTEM',
+	'sysuser'     => 'Konfigurasi PENGGUNA',
+	'data'     => 'Data',
+	'page'     => 'Halaman',
+	'flow'     => 'Aliran',
+	'nav'     => 'Navigasi',
+	'log'     => 'Log',
+	'navuserlist'     => 'Senarai Pengguna',
+	'navuserrole'     => 'Peranan Pengguna',
+
+);
