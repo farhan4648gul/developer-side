c45704a85b4004035030c698801b84a79d1eee13
joharijumali <joharijumali@gmail.com>
2013-07-18 04:13:23 +0000

    add error to layouts

--- bundles/admin/views/layouts/main.blade.php
+++ bundles/admin/views/layouts/main.blade.php
@@ -16,7 +16,7 @@
             width:      100%;
             opacity: 0.8;
             background: #000000 
-                        url("{{ url('img/loaders.gif'); }}")
+                        url("{{ url('bundles/admin/img/loader.gif'); }}")
                         50% 50% 
                         no-repeat;
         }
@@ -37,6 +37,47 @@
     </div>
     <div class="span10">
       <!--Body content-->
+      @if (isset($errors) && count($errors->all()) > 0)
+      <div class="alert alert-error">
+          <a class="close" data-dismiss="alert" href="#">×</a>
+          <h4 class="alert-heading">Oh Snap!</h4>
+          <ul>
+          @foreach ($errors->all('<li>:message</li>') as $message)
+          {{ $message }}
+          @endforeach
+          </ul>
+      </div>
+      @elseif (!is_null(Session::get('status_error')))
+      <div class="alert alert-error">
+          <a class="close" data-dismiss="alert" href="#">×</a>
+          <h4 class="alert-heading">Oh Snap!</h4>
+          @if (is_array(Session::get('status_error')))
+              <ul>
+              @foreach (Session::get('status_error') as $error)
+                  <li>{{ $error }}</li>
+              @endforeach
+              </ul>
+          @else
+              {{ Session::get('status_error') }}
+          @endif
+      </div>
+      @endif
+       
+      @if (!is_null(Session::get('status_success')))
+      <div class="alert alert-success">
+          <a class="close" data-dismiss="alert" href="#">×</a>
+          <h4 class="alert-heading">Success!</h4>
+          @if (is_array(Session::get('status_success')))
+              <ul>
+              @foreach (Session::get('status_success') as $success)
+                  <li>{{ $success }}</li>
+              @endforeach
+              </ul>
+          @else
+              {{ Session::get('status_success') }}
+          @endif
+      </div>
+      @endif
        @yield('content')
     </div>
   </div>
