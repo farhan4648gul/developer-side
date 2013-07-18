efbcfa54fd903f8bf68f84062d76b3d3b05a5c72
joharijumali@gmail.com <joharijumali@gmail.com@c5aebf30-8ea1-3613-f6ab-09ae8371c5c0>
2013-07-18 01:37:56 +0000

    reconfiguring dashboard

---
+++ trunk/bundles/admin/public/js/monitor/darwin.js
@@ -0,0 +1,12 @@
+
+
+function init() {
+
+	$('#LOAD').html('');
+	$('#LOAD').append('<div class="span12"><blockquote>Loading Server Load  &nbsp;<img src="bundles/admin/img/loader.gif"></blockquote><div id="server_loads"></div></div>');
+      			
+
+	$('#spacejump').html('');
+	$('#spacejump').append('<div class="accordion" id="accordion"></div>');
+
+}
