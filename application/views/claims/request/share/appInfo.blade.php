42386f34ac041b985287f35d1fe21a8cfc1da836
joharijumali <joharijumali@gmail.com>
2013-07-19 07:16:24 +0000

    request claim view

---
+++ application/views/claims/request/share/appInfo.blade.php
@@ -0,0 +1,28 @@
+<div class="row-fluid">
+  <div class="span12">
+    <table class="table table-bordered table-hover" >
+      <tbody>
+      <tr>
+        <td class="span1"><strong>Reference Number</strong></td>
+        <td class="span7 muted">{{$app['refno']}}</td>
+      </tr>
+      <tr>
+        <td class="span1"><strong>Claims Category</strong></td>
+        <td class="span7 muted">{{$app['category']}}</td>
+      </tr>
+      <tr>
+        <td class="span1"><strong>Date Apply</strong></td>
+        <td class="span7 muted">{{$app['applydate']}}</td>
+      </tr>
+      <tr>
+        <td class="span1"><strong>Month Apply For</strong></td>
+        <td class="span7 muted">{{$app['applymonth']}}</td>
+      </tr>
+      <tr>
+        <td class="span1"><strong>Status</strong></td>
+        <td class="span7 muted">{{$app['status']}}</td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+</div>
