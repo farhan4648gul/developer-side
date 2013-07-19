3e113a7d73e5889b0bffe27c5589d0e3b6bda489
joharijumali@gmail.com <joharijumali@gmail.com@c5aebf30-8ea1-3613-f6ab-09ae8371c5c0>
2013-07-19 07:18:18 +0000

    request claim view

--- trunk/application/views/claims/request/inforequestdetails.blade.php
+++ trunk/application/views/claims/request/inforequestdetails.blade.php
@@ -4,67 +4,10 @@
 <div class="page-header">
   <h3>Claims Request Information</h3>
 </div>
-<div class="row-fluid">
-  <div class="span12">
-    <table class="table table-bordered table-hover" >
-      <tbody>
-      <tr>
-        <td class="span1"><strong>Reference Number</strong></td>
-        <td class="span7 muted">{{$app['refno']}}</td>
-      </tr>
-      <tr>
-        <td class="span1"><strong>Claims Category</strong></td>
-        <td class="span7 muted">{{$app['category']}}</td>
-      </tr>
-      <tr>
-        <td class="span1"><strong>Date Apply</strong></td>
-        <td class="span7 muted">{{$app['applydate']}}</td>
-      </tr>
-      <tr>
-        <td class="span1"><strong>Month Apply For</strong></td>
-        <td class="span7 muted">{{$app['applymonth']}}</td>
-      </tr>
-      <tr>
-        <td class="span1"><strong>Status</strong></td>
-        <td class="span7 muted">{{$app['status']}}</td>
-      </tr>
-      </tbody>
-    </table>
-  </div>
-</div>
-<div class="row-fluid" >
-  <div class="span12">
-    <div id="dataList" class="rows-fluid show-grid">
-      {{ $detailList }}
-    </div>
-  </div>
-</div>
 
+@include('claims.request.share.appInfo')
+@include('claims.request.share.detailList')
 
-<!-- Modal -->
-
-<div id="addResitModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-<div class="modal-header">
-  <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-  <h3 id="myModalLabel">Uploaded Receipt</h3>
-</div>
-<div class="modal-body">
-<div id="recCoru" class="carousel slide">
-  <!-- Carousel items -->
-  <div class="carousel-inner">
-    <div class="active item"><img src="{{ url('upload/claims/356a192b7913b04c54574d18c28d46e6395428ab/20130623/1/13/vZgIJVcphiodMOdD1372013927.png')}}" alt=""></div>
-    <div class="item"><img src="{{ url('upload/claims/356a192b7913b04c54574d18c28d46e6395428ab/20130623/1/13/vZgIJVcphiodMOdD1372013927.png')}}" alt=""></div>
-    <div class="item"><img src="{{ url('upload/claims/356a192b7913b04c54574d18c28d46e6395428ab/20130623/1/13/vZgIJVcphiodMOdD1372013927.png')}}" alt=""></div>
-  </div>
-  <!-- Carousel nav -->
-  <a class="carousel-control left" href="#recCoru" data-slide="prev">&lsaquo;</a>
-  <a class="carousel-control right" href="#recCoru" data-slide="next">&rsaquo;</a>
-</div>
-</div>
-<div class="modal-footer">
-  <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
-</div>
-</div>
 
 @endsection
 @section('scripts')
