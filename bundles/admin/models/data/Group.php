872f260ba0958284e2a5d66f547467a54f869591
joharijumali <joharijumali@gmail.com>
2013-07-23 07:01:14 +0000

    data group edit and delete

--- bundles/admin/models/data/Group.php
+++ bundles/admin/models/data/Group.php
@@ -1,7 +1,7 @@
 <?php namespace Admin\Models\Data;
 use \Laravel\Database\Eloquent\Model as Eloquent,
 	Datagrid, Laravel\Config as Config,
-    Laravel\Database as DB;
+    Laravel\Database as DB,Laravel\Log,Laravel\Str,Laravel\Lang;
 
 class Group extends Eloquent {
 
@@ -11,13 +11,13 @@ class Group extends Eloquent {
 
     public static function listData(){
 
-        $allGroup = Group::paginate(10);
+        $allGroup = Group::paginate(Config::get('system.pagination'));
 
         $datagrid = new Datagrid;
-        $datagrid->setFields(array('group_name' =>'Data Group'));
-        $datagrid->setAction('manage','datacontent',false);
-        $datagrid->setAction('edit','deleteRole',true,array('dmid'));
-        $datagrid->setAction('delete','deleteRole',true,array('dmid'));
+        $datagrid->setFields(array('group_name' => Str::upper(Lang::line('admin.datagroup')->get())));
+        $datagrid->setAction(Lang::line('global.manage')->get(),'datacontent',false);
+        $datagrid->setAction(Lang::line('global.edit')->get(),'editGroup',true,array('dmid'));
+        $datagrid->setAction(Lang::line('global.delete')->get(),'deleteGroup',true,array('dmid','group_name'));
         $datagrid->setTable('dataGroup','table table-bordered table-hover table-striped table-condensed');
         $datagrid->build($allGroup,'dmid');
 
@@ -25,7 +25,17 @@ class Group extends Eloquent {
 
     }
 
+    public static function getGroupinfo($groupID){
 
+        $dataGroup = Group::find($groupID);
+
+        $data['groupid'] = $dataGroup->dmid;
+        $data['group_name'] = $dataGroup->group_name;
+        $data['group_model'] = $dataGroup->group_model;
+        $data['group_key'] = $dataGroup->group_key;
+
+        return $data;
+    }
 
     public static function getDataModel($groupID){
 
@@ -76,6 +86,41 @@ class Group extends Eloquent {
     public static function getDataHeader(){
 
         return Content::$header;
+    }
+
+
+    /**
+     * Check Existing Data
+     *
+     * @return void
+     * @author 
+     **/
+    public static function checkData($table = NULL){
+        
+        $existTable = DB::query("show tables like '".Content::$table."'");
+
+        if(empty($existTable)) return false;
+
+        $table = ($table != NULL)?$table:Content::$table;
+        $count = DB::table($table)->count();
+
+        return ($count>0)?true:false;
+
+    }
+
+    /**
+     * Check Existing table
+     *
+     * @return void
+     * @author 
+     **/
+    public static function checkTable($table,$field){
+
+        $existTable = DB::query("show tables like '".$table."'");
+        $tableCol = DB::query("show columns from ".$table." like '".$field."'");
+
+        return (!empty($existTable) && !empty($tableCol))?true:false;
+
     }
 
 }
