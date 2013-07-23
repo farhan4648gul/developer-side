6a3aacd380db34371ec282ab000b64a0bacfc508
joharijumali@gmail.com <joharijumali@gmail.com@c5aebf30-8ea1-3613-f6ab-09ae8371c5c0>
2013-07-23 07:06:31 +0000

    add search n edit user info

--- trunk/bundles/admin/views/user/role.blade.php
+++ trunk/bundles/admin/views/user/role.blade.php
@@ -2,10 +2,10 @@
 
 @section('content')
 <div class="page-header">
-    <h3>Role Management</h3>
+    <h3>{{ Str::upper(Lang::line('admin.sysrole')->get()) }}</h3>
 </div> 	
 <div class="row-fluid">
-	<a href="#addRoleForm" role="button" class="btn pull-right" data-toggle="modal" style='margin-bottom:10px'><i class="icon-plus"></i>&nbsp;New Role</a>
+	<a href="#addRoleForm" role="button" class="btn pull-right" data-toggle="modal" style='margin-bottom:10px'><i class="icon-plus"></i>&nbsp;{{ Str::upper(Lang::line('admin.addnewrole')->get()) }}</a>
 </div>
 <div id="roleList" class="row-fluid">
 	{{ $rolelist }}
@@ -14,56 +14,56 @@
 <div id="addRoleForm" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
 <div class="modal-header">
 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-<h3 id="myModalLabel">Add New Role</h3>
+<h3 id="myModalLabel">{{ Str::upper(Lang::line('admin.addnewrole')->get()) }}</h3>
 </div>
 <div class="modal-body">
 {{ Form::open('admin/user/role', 'POST', array('id' => 'roleAddForm', 'class' => 'form-horizontal')) }}
 {{ Form::hidden('roleid') }}
   <div class="control-group">
-    <label class="control-label" for="role">Role</label>
+    <label class="control-label" for="role">{{ Str::upper(Lang::line('admin.rolename')->get()) }}</label>
     <div class="controls">
-      {{ Form::xlarge_text('role',null,array('placeholder'=>'Type Role Name','required')) }}
+      {{ Form::xlarge_text('role',null,array('placeholder'=>Str::title(Lang::line('global.type')->get().' '.Lang::line('admin.rolename')->get()),'required')) }}
     </div>
   </div>
   <div class="control-group">
-    <label class="control-label" for="roleDesc">Description</label>
+    <label class="control-label" for="roleDesc">{{ Str::upper(Lang::line('global.desc')->get()) }}</label>
     <div class="controls">
-      {{ Form::xlarge_text('roledesc',null,array('placeholder'=>'Role Description')) }}
+      {{ Form::xlarge_text('roledesc',null,array('placeholder'=>Str::title(Lang::line('global.type')->get().' '.Lang::line('admin.roledesc')->get()))) }}
     </div>
   </div>
 {{ Form::close()}}
 </div>
 <div class="modal-footer">
-<button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
-<button id="addBtn" class="btn btn-primary" data-loading-text="Saving Role">Save Role</button>
+<button class="btn" data-dismiss="modal" aria-hidden="true">{{ Str::title(Lang::line('global.close')->get()) }}</button>
+<button id="addBtn" class="btn btn-primary" data-loading-text="Saving Role">{{ Str::title(Lang::line('global.save')->get()) }}</button>
 </div>
 </div>
 
 <div id="editRoleModal" class="modal hide fade" tabindex="-2" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
 <div class="modal-header">
 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-<h3 id="myModalLabel">Edit Role</h3>
+<h3 id="myModalLabel">{{ Str::upper(Lang::line('admin.editrole')->get()) }}</h3>
 </div>
 <div class="modal-body">
 {{ Form::open('admin/user/role', 'POST', array('id' => 'roleUpdateForm', 'class' => 'form-horizontal')) }}
 {{ Form::hidden('roleid') }}
   <div class="control-group">
-    <label class="control-label" for="role">Role</label>
+    <label class="control-label" for="role">{{ Str::upper(Lang::line('admin.rolename')->get()) }}</label>
     <div class="controls">
-      {{ Form::xlarge_text('role',null,array('placeholder'=>'Type Role Name')) }}
+      {{ Form::xlarge_text('role',null,array('placeholder'=>Str::title(Lang::line('global.type')->get().' '.Lang::line('admin.rolename')->get()),'required')) }}
     </div>
   </div>
   <div class="control-group">
-    <label class="control-label" for="roleDesc">Description</label>
+    <label class="control-label" for="roleDesc">{{ Str::upper(Lang::line('global.desc')->get()) }}</label>
     <div class="controls">
-    	{{ Form::xlarge_text('roledesc',null,array('placeholder'=>'Role Description')) }}
+    	{{ Form::xlarge_text('roledesc',null,array('placeholder'=>Str::title(Lang::line('global.type')->get().' '.Lang::line('admin.roledesc')->get()))) }}
     </div>
   </div>
 {{ Form::close()}}
 </div>
 <div class="modal-footer">
-<button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
-<button id="editBtn" class="btn btn-primary"  data-loading-text="Updating Role">Update Role</button>
+<button class="btn" data-dismiss="modal" aria-hidden="true">{{ Str::title(Lang::line('global.close')->get()) }}</button>
+<button id="editBtn" class="btn btn-primary"  data-loading-text="Updating Role">{{ Str::title(Lang::line('global.edit')->get()) }}</button>
 </div>
 </div>
 
