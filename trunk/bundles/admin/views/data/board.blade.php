9c6c226bd901afdd269868c1c2d9e333d7fb6656
joharijumali@gmail.com <joharijumali@gmail.com@c5aebf30-8ea1-3613-f6ab-09ae8371c5c0>
2013-07-23 07:03:58 +0000

    data group edit and delete

--- trunk/bundles/admin/views/data/board.blade.php
+++ trunk/bundles/admin/views/data/board.blade.php
@@ -2,11 +2,11 @@
 
 @section('content')
 <div class="page-header">
-    <h3>Data Management</h3>
+    <h3>{{ Str::upper(Lang::line('admin.datamanagement')->get()) }}</h3>
 </div>
 <div class="row-fluid">
   <div class="pull-right">
-    <a href="#addDataModal" role="button" class="btn" data-toggle="modal" style='margin-bottom:10px'><i class="icon-plus"></i>&nbsp;Add New Data Group</a>
+    <a href="#addDataModal" role="button" class="btn" data-toggle="modal" style='margin-bottom:10px'><i class="icon-plus"></i>&nbsp;{{ Str::title(Lang::line('admin.adddatagroup')->get()) }}</a>
   </div>
 </div>
 
@@ -17,18 +17,19 @@
 <div id="addDataModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
 <div class="modal-header">
   <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-  <h3 id="myModalLabel">Add New Data Group</h3>
+  <h3 id="myModalLabel">{{ Str::title(Lang::line('admin.adddatagroup')->get()) }}</h3>
 </div>
 <div class="modal-body">
 {{ Form::open('admin/user/role', 'POST', array('id' => 'addDataForm', 'class' => 'form-horizontal')) }}
-{{ Form::control_group(Form::label('table', 'Data Group Name'),Form::large_text('group_name',null,array('placeholder'=>'Type Data Group Name','required'))) }}
-{{ Form::control_group(Form::label('group_model', 'Table Name'),Form::large_text('group_model', '', array('required'))) }}
-{{ Form::control_group(Form::label('group_key', 'Primary Key ID'),Form::large_text('group_key', '', array('required'))) }}
+{{ Form::hidden('groupid',NULL)}}
+{{ Form::control_group(Form::label('group_name', Str::title(Lang::line('admin.datagroupname')->get())),Form::large_text('group_name',null,array('placeholder'=>Str::title(Lang::line('global.type')->get().' '.Lang::line('admin.datagroupname')->get()),'required'))) }}
+{{ Form::control_group(Form::label('group_model', Str::title(Lang::line('admin.datagroupmodel')->get())),Form::large_text('group_model', '', array('placeholder'=>Str::title(Lang::line('global.type')->get().' '.Lang::line('admin.datagroupmodel')->get()),'required'))) }}
+{{ Form::control_group(Form::label('group_key', Str::title(Lang::line('admin.datagroupkey')->get())),Form::large_text('group_key', '', array('placeholder'=>Str::title(Lang::line('global.type')->get().' '.Lang::line('admin.datagroupkey')->get()),'required'))) }}
 {{ Form::close()}}
 </div>
 <div class="modal-footer">
-  <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
-  <button id="addDataGroupBtn" class="btn btn-primary">Save</button>
+  <button class="btn" data-dismiss="modal" aria-hidden="true">{{ Str::title(Lang::line('global.close')->get()) }}</button>
+  <button id="addDataGroupBtn" class="btn btn-primary">{{ Str::title(Lang::line('global.save')->get()) }}</button>
 </div>
 </div>
 @endsection
@@ -40,12 +41,50 @@
       $.post('datagroup', $("#addDataForm").serialize(),function(data) {
               sourcedata = data;
             }).success(function() { 
-              $( "#dataGroupList" ).empty().html( sourcedata );
-              $("#addDataForm :input").val('');
-              $('#addDataModal').modal('hide');
+              sourcedata = jQuery.parseJSON(sourcedata);
+              console.log(sourcedata);
+              if(sourcedata.fail){
+                alert(sourcedata.fail);
+              }else{
+                $( "#dataGroupList" ).empty().html( sourcedata );
+                $("#addDataForm :input").val('');
+                $('#addDataModal').modal('hide');
+              }
+                
             });
 
     });
+
+    function editGroup(id){
+      $('#addDataModal').modal('show');
+      $('.modal-header #myModalLabel').text("{{ Str::title(Lang::line('admin.editdatagroup')->get()) }}");
+      $.get('groupinfo', { groupid: id},function(data,status){
+        for (x in data)
+        {   
+          $('#addDataForm input[name="'+ x +'"]' ).val(data[x]);
+
+        }
+      },"json");
+    }
+
+  function deleteGroup(id,name){
+
+    var r = confirm("{{ Str::title(Lang::line('admin.deletegroup')->get()) }}" +name.toUpperCase());
+
+    if (r==true){
+        $.post("deleteGroup", "groupid="+id+"&name="+name ,function(data) {
+              sourcedata = data;
+            }).success(function() {
+            sourcedata = jQuery.parseJSON(sourcedata);
+            if(sourcedata.fail){
+              alert(sourcedata.fail);
+            }else{
+              $( "#dataGroupList" ).empty().append( sourcedata );
+            }
+              });
+    }
+
+  }
 
 </script>
 @endsection
