e6a1f7b669b96a9c6bfad711f60e43bed3bb3f4f
joharijumali@gmail.com <joharijumali@gmail.com@c5aebf30-8ea1-3613-f6ab-09ae8371c5c0>
2013-07-23 07:03:31 +0000

    add search n edit user info

--- trunk/bundles/admin/models/user/User.php
+++ trunk/bundles/admin/models/user/User.php
@@ -4,7 +4,8 @@ use \Laravel\Database\Eloquent\Model as Eloquent,
 	Datagrid as Datagrid,
     Laravel\Hash as Hash,
     Laravel\Log as Log,
-	Admin\Models\User\Profile as Profile;
+    Laravel\Auth as Auth,
+	Admin\Models\User\Profile as Profile,Laravel\Str,Laravel\Lang,Laravel\Config;
 
 class User extends Eloquent {
 
@@ -14,8 +15,7 @@ class User extends Eloquent {
 
     public static $rules = array( 
                                 'username' => 'required|unique:users', 
-                                'password' => 'required', 
-                                'role' => ''
+                                'password' => 'required',
                             );
 
     
@@ -35,15 +35,42 @@ class User extends Eloquent {
     
     public static function listUser(){
 
-        $allUser = User::where('role','<>','0')->paginate(10);
+        $allUser = User::where('role','<>','0')->paginate(Config::get('system.pagination'));
 
         $datagrid = new Datagrid;
-        $datagrid->setFields(array('userprofile/fullname' =>'Fullname'));
-        $datagrid->setFields(array('userprofile/emel' =>'Email'));
-        $datagrid->setFields(array('userprofile/icno' =>'IC Number'));
-        $datagrid->setFields(array('status' =>'Status'));
-        $datagrid->setAction('view','deleteRole',true,array('userid'));
-        $datagrid->setAction('reset','deleteRole',true,array('userid'));
+        $datagrid->setFields(array('userprofile/fullname' => Str::upper(Lang::line('admin.fullname')->get())));
+        $datagrid->setFields(array('userprofile/emel' => Str::upper(Lang::line('admin.activeemel')->get())));
+        $datagrid->setFields(array('userprofile/icno' => Str::upper(Lang::line('admin.idno')->get())));
+        $datagrid->setFields(array('status' => Str::upper('Status')));
+        $datagrid->setAction(Lang::line('global.edit')->get(),'viewUser',true,array('userid'));
+        $datagrid->setAction(Lang::line('global.delete')->get(),'deleteAccount',true,array('userid','userprofile/fullname'));
+        $datagrid->setAction(Lang::line('global.reset')->get(),'resetAccount',true,array('userid'));
+        $datagrid->setTable('users','table table-bordered table-hover table-striped table-condensed');
+        $datagrid->build($allUser,'userid');
+
+        return $datagrid->render();
+
+    }    
+
+    public static function userSearchList($input){
+
+        $operator = (stripos($input['searchval'], '*'))? 'LIKE':'=' ;
+        $val = str_replace("*", "", $input['searchval']);
+        $refval = (stripos($input['searchval'], '*'))? '%'.$val.'%':$val ;
+
+        $allUser = User::left_join('users_profiles', 'users.userid', '=', 'users_profiles.userid')
+                    ->where('icno', $operator, $refval)
+                    ->or_where('fullname', $operator, $refval)
+                    ->paginate(Config::get('system.pagination'));
+
+        $datagrid = new Datagrid;
+        $datagrid->setFields(array('userprofile/fullname' => Str::upper(Str::title(Lang::line('admin.fullname')->get()))));
+        $datagrid->setFields(array('userprofile/emel' => Str::upper(Str::title(Lang::line('admin.activeemel')->get()))));
+        $datagrid->setFields(array('userprofile/icno' => Str::upper(Str::title(Lang::line('admin.idno')->get()))));
+        $datagrid->setFields(array('status' => Str::upper('Status')));
+        $datagrid->setAction(Lang::line('global.edit')->get(),'viewUser',true,array('userid'));
+        $datagrid->setAction(Lang::line('global.delete')->get(),'deleteAccount',true,array('userid'));
+        $datagrid->setAction(Lang::line('global.reset')->get(),'resetAccount',true,array('userid'));
         $datagrid->setTable('users','table table-bordered table-hover table-striped table-condensed');
         $datagrid->build($allUser,'userid');
 
@@ -109,6 +136,51 @@ class User extends Eloquent {
 
         
     }
+
+     public static function userinfo($id){
+
+        $userInfo = User::find($id);
+
+        $profile = $userInfo->userprofile;
+
+        $role = $userInfo->roles;
+
+        $data['username'] = $userInfo->username;
+        $data['fullname'] = $profile->fullname;
+        $data['icno'] = $profile->icno;
+        $data['emel'] = $profile->emel;
+        $data['role'] = $userInfo->role;
+        $data['status'] = $userInfo->status;
+        $data['userid'] = $userInfo->userid;
+
+        return $data;
+
+     }
+
+     public static function updateUser($input){
+
+        $user = User::find($input['userid']);
+        $profileid = $user->userprofile->profileid;
+        $user->role = $input['role'];
+        $user->status = $input['status'];
+        $user->save();
+
+        $prof = array(array('fullname' => $input['fullname'],
+                    'icno' => $input['icno'],
+                    'emel' => $input['emel'],
+                ));
+
+        $profile = Profile::find($profileid);
+        $profile->fullname = $input['fullname'];
+        $profile->icno = $input['icno'];
+        $profile->emel = $input['emel'];
+        $profile->save();
+
+        $profile = User::find($input['userid']);
+
+        Log::write('User', 'Update User ' . $user->userprofile->icno. 'By '.Auth::user()->username);
+
+     }
 
 
 }
