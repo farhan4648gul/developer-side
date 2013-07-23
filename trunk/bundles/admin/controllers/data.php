4d0bffdf81e282ff69fda5797d6e09c8fb4309ff
joharijumali@gmail.com <joharijumali@gmail.com@c5aebf30-8ea1-3613-f6ab-09ae8371c5c0>
2013-07-23 06:58:40 +0000

    edit data management

--- trunk/bundles/admin/controllers/data.php
+++ trunk/bundles/admin/controllers/data.php
@@ -1,7 +1,7 @@
 <?php
 
 use Admin\Models\Data\Group as Group,
-    Logger as Logger,
+    Logger as Logger,Laravel\Log,
     Admin\Libraries\Crapcrush as Crapcrush;
 
 /**
@@ -34,18 +34,55 @@ class Admin_Data_Controller extends Admin_Base_Controller {
 
         $input = Input::get();
 
-        // if($input['roleid'] == NULL):
-        $dataGroup = new Group;
-        // else:
-        //  $role = Admin_UserRole::find($input['roleid']);
-        // endif;
-        $dataGroup->group_name  = Str::upper($input['group_name']);
-        $dataGroup->group_model  = $input['group_model'];
-        $dataGroup->group_key  = $input['group_key'];
-        $dataGroup->save();
+        $existed = Group::checkTable($input['group_model'],$input['group_key']);
+        
+        if($existed){
+	        if($input['groupid'] == NULL):
+	        $dataGroup = new Group;
+	        else:
+	         $dataGroup = Group::find($input['groupid']);
+	        endif;
+	        $dataGroup->group_name  = Str::upper($input['group_name']);
+	        $dataGroup->group_model  = $input['group_model'];
+	        $dataGroup->group_key  = $input['group_key'];
+	        $dataGroup->save();
 
-		return Group::listData();
+	        return json_encode(Group::listData());
+        }else{
+        	return json_encode(array('fail'=>Str::title(Lang::line('admin.groupfail')->get())));
+        }
+
+
+
 	}
+
+	public function get_groupinfo(){
+
+		$input = Input::get();
+
+		return json_encode(Group::getGroupinfo($input['groupid']));
+	}
+
+    public function post_deleteGroup(){
+
+        $groupid = Input::get('groupid');
+        $name = Input::get('name');
+
+        Group::getDataModel($groupid);
+
+        $existed = Group::checkData();
+
+        if(!$existed){
+            Group::find($groupid)->delete();
+
+            Log::write('Data', 'Data Group<b>'.$name.'</b> Remove by '.Auth::user()->username);
+
+            return json_encode(Group::listData());
+        }else{
+            return json_encode(array('fail'=>Str::title(Lang::line('admin.deletegroupfail')->get())));
+        }
+
+    }	
 
 	public function post_datainput(){
 
